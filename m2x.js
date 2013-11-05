0e8b9f04f27823907dc59f96063d064f231da838
Gabriel Garcia <seriousmoth@gmail.com>
2013-11-05 13:53:01 -0200

    Initial commit with code. WIP

---
+++ m2x.js
@@ -0,0 +1,93 @@
+'use strict';
+
+var request = require("request");
+var querystring = require("querystring");
+var API_BASE = "http://api-m2x.att.com/v1";
+var USER_AGENT = "M2X/0.0.2 (node.js)";
+
+
+var Utils = {
+    extend: function(target) {
+        var sources = [].slice.call(arguments, 1);
+        sources.forEach(function(source) {
+            var prop;
+            for (prop in source) {
+                target[prop] = source[prop];
+            }
+        });
+        return target;
+    }
+};
+
+
+var Feeds = function() {
+};
+
+
+var Keys = function() {
+};
+
+
+var Client = function(apiKey, apiBase) {
+    this.apiKey = apiKey;
+    this.apiBase = apiBase || API_BASE;
+
+    this.defaultHeaders = {
+        "User-Agent": USER_AGENT,
+        "X-M2X-KEY": this.apiKey
+    };
+
+    // Define request methods by verb
+    ['get', 'post', 'put', 'delete', 'head', 'options', 'patch'].
+        forEach(function(verb) {
+            this[verb] = function(path, qs, params, headers) {
+                this.request(verb, path, qs, params, headers);
+            };
+        }.bind(this));
+};
+
+Client.prototype.request = function(verb, path, qs, params, headers) {
+    var body;
+
+    headers = Utils.extend(this.defaultHeaders, headers || {});
+
+    if (! headers["Content-Type"]) {
+        headers["Content-Type"] = "application/x-www-form-urlencoded";
+    }
+
+    if (params) {
+        switch (headers["Content-Type"]) {
+        case "application/json":
+            body = JSON.stringify(params);
+            break;
+        case "application/x-www-form-urlencoded":
+            body = querystring.stringify(params);
+            break;
+        default:
+            throw "Unrecognized Content-Type `" + headers["Content-Type"] + "`";
+        }
+    }
+
+    request.request({
+        url: this.apiBase + path,
+        qs: qs,
+        method: verb,
+        headers: headers,
+        body: body
+    }, function(error, response, body) {
+        console.log(error);
+        console.log(response);
+        console.log(body);
+    });
+};
+
+
+var M2X = function(apiKey, apiBase) {
+    this.client = new Client(apiKey, apiBase);
+    this.keys = new Keys(this.client);
+    this.feeds = new Feeds(this.client);
+};
+
+M2X.prototype.status = function() {
+    return this.client.get("/status");
+};
