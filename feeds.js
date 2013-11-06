db2f0896ebdf277ff48b8553161336cdeb85b4e2
Gabriel Garcia <seriousmoth@gmail.com>
2013-11-06 15:34:17 -0200

    Added explanatory comments to Feeds class methods.

--- feeds.js
+++ feeds.js
@@ -2,6 +2,10 @@
 
 var util = require('util');
 
+// Wrapper for AT&T M2X Feed API
+//
+// See https://m2x.att.com/developer/documentation/feed for AT&T M2X
+// HTTP Feed API documentation.
 var Feeds = function(client) {
     this.client = client;
 };
@@ -14,54 +18,78 @@ Feeds.prototype.url = function(format) {
     return util.format.apply(this, [format].concat(params));
 };
 
+// List all the feeds that belong to the user associated with the
+// M2X API key supplied when initializing M2X
 Feeds.prototype.list = function() {
     return this.client.get("/feeds");
 };
 
+// Return the details of the supplied feed
 Feeds.prototype.view = function(id) {
     return this.client.get(this.url("/feeds/%s", id));
 };
 
+// Return a list of access log to the supplied feed
 Feeds.prototype.log = function(id) {
     return this.client.get(this.url("/feeds/%s/log", id));
 };
 
+// Return the current location of the supplied feed
+//
+// Note that this method can return an empty value (response status
+// of 204) if the feed has no location defined.
 Feeds.prototype.location = function(id) {
     return this.client.get(this.url("/feeds/%s/location", id));
 };
 
+// Update the current location of the feed
 Feeds.prototype.updateLocation = function(id, params) {
     return this.client.put(this.url("/feeds/%s/location", id), null, params);
 };
 
+// Return a list of the associated streams for the supplied feed
 Feeds.prototype.streams = function(id) {
     return this.client.get(this.url("/feeds/%s/streams", id));
 };
 
+// Return the details of the supplied stream
 Feeds.prototype.stream = function(id, name) {
     return this.client.get(this.url("/feeds/%s/streams/%s", id, name));
 };
 
+// Return a list with the latest values from a stream
 Feeds.prototype.streamValues = function(id, name) {
     return this.client.get(this.url("/feeds/%s/streams/%s/values", id, name));
 };
 
+// Update stream's properties
+//
+// If the stream doesn't exist it will create it. See
+// https://m2x.att.com/developer/documentation/feed#Create-Update-Data-Stream
+// for details.
 Feeds.prototype.updateStream = function(id, name, params) {
     return this.client.put(this.url("/feeds/%s/streams/%s", id, name), null, params);
 };
 
+// Delete the stream (and all its values) from the feed
 Feeds.prototype.deleteStream = function(id, name) {
     return this.client.del(this.url("/feeds/%s/streams/%s", id, name));
 };
 
+// Returns a list of API keys associated with the feed
 Feeds.prototype.keys = function(id) {
     return this.client.get("/keys", { feed: id });
 };
 
+// Creates a new API key associated to the feed
+//
+// If a parameter named `stream` is supplied with a stream name, it
+// will create an API key associated with that stream only.
 Feeds.prototype.createKey = function() {
     // TODO
 };
 
+// Updates an API key properties
 Feeds.prototype.updateKey = function() {
     // TODO
 };
