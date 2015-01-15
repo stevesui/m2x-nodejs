b367c63c5de198a64f1eb8a73438b5ab12140fcc
Gonzalo Arreche <gonzaloarreche@gmail.com>
2015-01-15 13:12:51 -0200

    Add function to create multiple data streams at once

--- examples/uptime.js
+++ examples/uptime.js
@@ -13,28 +13,33 @@ var m2xClient = new M2X(config.api_key);
 
 // Create the streams if they don't exist already
 source.update(function(data) {
-    m2xClient.devices.updateStream(config.device, "load_1m");
-    m2xClient.devices.updateStream(config.device, "load_5m");
-    m2xClient.devices.updateStream(config.device, "load_15m");
-});
+    var streams = ["load_1m", "load_5m", "load_15m"];
 
-// Retrieve values each 1100ms and post them to the device
-source.updateEvery(1100, function(data, stopLoop) {
-    var at = new Date().toISOString();
-    var values = {
-        load_1m:  [ { value: data.load_1m, timestamp: at } ],
-        load_5m:  [ { value: data.load_5m, timestamp: at } ],
-        load_15m: [ { value: data.load_15m, timestamp: at } ]
-    };
-
-    // Write the different values into AT&T M2X
-    m2xClient.devices.postMultiple(config.device, values, function(result) {
-        console.log(result);
-        if (result.isError()) {
-            // Stop the update loop if an error occurs.
-            stopLoop();
-
-            console.log(result.error());
+    m2xClient.devices.updateStreams(config.device, streams, function(response) {
+        // Retrieve values each 1100ms and post them to the device
+        if (response.isError()) {
+            console.log("Cannot create stream:", response);
+            return;
         }
+        source.updateEvery(1000, function(data, stopLoop) {
+            var at = new Date().toISOString();
+            var values = {
+                load_1m:  [ { value: data.load_1m, timestamp: at } ],
+                load_5m:  [ { value: data.load_5m, timestamp: at } ],
+                load_15m: [ { value: data.load_15m, timestamp: at } ]
+            };
+
+            // Write the different values into AT&T M2X
+            m2xClient.devices.postMultiple(config.device, values, function(result) {
+                console.log(result);
+                if (result.isError()) {
+                    // Stop the update loop if an error occurs.
+                    stopLoop();
+
+                    console.log(result.error());
+                }
+            });
+        });
     });
 });
+
